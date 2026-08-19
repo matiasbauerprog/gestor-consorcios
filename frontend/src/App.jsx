a3c638951ef1b920f75a1063a5e11c31fd22d6a5
matiasbauerprog <matiasbauer@gmail.com>
2026-08-18 21:40:08 -0300

    feat: pantalla de preferencias de avisos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ import SuperAdminAuditLog from "./screens/SuperAdminAuditLog";
 import SuperAdminErrores from "./screens/SuperAdminErrores";
 import Comunicados from "./screens/Comunicados";
 import Notificaciones from "./screens/Notificaciones";
+import PreferenciasAvisos from "./screens/PreferenciasAvisos";
 import Expensas from "./screens/Expensas";
 import Comprobantes from "./screens/Comprobantes";
 import Gastos from "./screens/Gastos";
@@ -137,6 +138,7 @@ export default function App() {
             <Route index element={<InicioRoute />} />
             <Route path="comunicados" element={<Comunicados />} />
             <Route path="notificaciones" element={<Notificaciones />} />
+            <Route path="notificaciones/preferencias" element={<PreferenciasAvisos />} />
             <Route path="expensas" element={<ExpensasRoute />} />
             <Route path="mi-cuenta" element={<MiCuentaRoute />} />
             <Route path="departamentos/:id/cuenta" element={<DepartamentoCuenta />} />
